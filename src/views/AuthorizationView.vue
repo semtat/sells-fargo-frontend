f0e69f52c12dd723cbf364b60ba76b6c6378f2e1
Semen <semen.tatarnikov@gmail.com>
2022-09-14 14:56:21 +0300

    [SF-29] Верстка страницы аутентификации

--- src/views/AuthorizationView.vue
+++ src/views/AuthorizationView.vue
@@ -1,47 +1,21 @@
 <template>
-    <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <div class="w-full max-w-md space-y-8">
-        <div>
-            <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
-            <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
-            <p class="mt-2 text-center text-sm text-gray-600">
-            Or
-            {{ ' ' }}
-            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">start your 14-day free trial</a>
-            </p>
-        </div>
-        <form class="mt-8 space-y-6" action="#" method="POST">
-            <input type="hidden" name="remember" value="true" />
-            <div class="-space-y-px rounded-md shadow-sm">
-            <div>
-                <label for="email-address" class="sr-only">Email address</label>
-                <input id="email-address" name="email" type="email" autocomplete="email" required class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Email address" />
-            </div>
-            <div>
-                <label for="password" class="sr-only">Password</label>
-                <input id="password" name="password" type="password" autocomplete="current-password" required class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" placeholder="Password" />
-            </div>
-            </div>
-
-            <div class="flex items-center justify-between">
-            <div class="flex items-center">
-                <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500" />
-                <label for="remember-me" class="ml-2 block text-sm text-gray-900">Remember me</label>
-            </div>
-
-            <div class="text-sm">
-                <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Forgot your password?</a>
-            </div>
-            </div>
-
-            <div>
-            <button type="submit" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                <span class="absolute inset-y-0 left-0 flex items-center pl-3">
-                </span>
-                Sign in
-            </button>
-            </div>
-        </form>
+    <div class="flex container h-screen bg-green-500 items-center justify-center">
+        <div class="w-full shadow-2xl max-w-md p-10 bg-white">
+            <form action="#" method="POST">
+                <div>
+                    <input id="username" name="username" type="text" placeholder="username"  required 
+                        class="relative block mb-4 w-full appearance-none rounded-none p-4 bg-zinc-100 text-gray-900 placeholder-zinc-400 focus:z-10" />
+                    <input id="password" name="password" type="password" autocomplete="current-password" placeholder="password" required 
+                        class="relative block mb-4 w-full appearance-none rounded-none p-4 bg-zinc-100 text-gray-900 placeholder-zinc-400 focus:z-10" />
+                    <button type="submit" 
+                        class="relative flex mb-4 w-full justify-center bg-green-600 p-4 text-white hover:bg-green-700">
+                        <span>LOGIN</span>
+                    </button>
+                    <div class="text-center text-zinc-400">
+                        <span>Not registered?</span> <a href="#" class="text-green-500">Create an account</a>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 </template>
